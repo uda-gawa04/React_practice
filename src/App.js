ab3c210b3a5e7a08c775a2df839d4049f1f4f43d
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-06 09:24:49 +0900

    Reactの基本

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [count, setConst] = useState(0);
+  const onClickCountup = () => {
+    setConst(count + 1);
+  };
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      {/* 変数を表示 */}
+      <p>{count}</p>
+      {/* ボタンを押すと、count変数が変わる機能を持った変数を呼び出す */}
+      <button onClick={onClickCountup}>countup</button>
     </div>
   );
 }
