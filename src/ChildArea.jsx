2941031e747f452f4b756c3f430ae951b539ef55
uda-gawa04 <108511181+uda-gawa04@users.noreply.github.com>
2022-07-06 10:51:41 +0900

    レンダリングの最適化１

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,15 +1,25 @@
+import { memo } from "react";
+
 const style = {
   width: "500px",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
   // 分割代入
   const { open } = props;
+
+  // 模擬的にレンダリングの不可を高める
+  // 2000件のデータが入った配列を作る
+  const data = [...Array(2000).keys()];
+  // データをめちゃ作る
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
-      {/* 受け取ったpropsがtrueなら左、falseなら右をレンダリング */}
       {open ? (
         <div style={style}>
           <p>ChildConponent</p>
@@ -17,4 +27,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
